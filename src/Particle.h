0ebf0936f376736c49f428c9727746ae89d79ee7
jptorrezan <jptorrezan@gmail.com>
2022-01-29 13:51:22 -0300

    particles moviment finished

--- src/Particle.h
+++ src/Particle.h
@@ -5,15 +5,17 @@ struct Particle {
     double m_x;
     double m_y;
 
+private:
     double m_speed;
     double m_direction;
+     void init();
 
 
 public:
     Particle();
     virtual ~Particle();
     void update(int interval);
-
+   
 };
 
 
